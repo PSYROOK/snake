6955801b168edb2a807219ae9f16c522e3a21232
PSYROOK <kga16q@gmail.com>
2020-04-26 21:33:02 +0300

    functoins

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,8 +10,24 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world");
+            int x1 = 1;
+            int y1 = 3;
+            char sym1 = '*';
+
+            Draw(x1, y1, sim1);
+
+            int x2 = 4;
+            int y2 = 5;
+            char sym2 = '#';
+
+            Draw(x2, y2, sim2);
+
             Console.ReadLine();
+        }
+
+        static void Draw(int x, int y, sim) {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sim);
         }
     }
 }
